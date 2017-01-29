e94a03147febd40f5ef5fc6dddb956e56b014401
ackepenek <ahmetcan.kepenek@gmail.com>
2017-01-29 23:29:00 +0300

    userprofile admin panele eklendi

--- userprofile/admin.py
+++ userprofile/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from userprofile.models import UserProfile
+
+
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    pass
